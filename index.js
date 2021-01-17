3e1c9ea5d32fb3a49aa7a7e4ff169bc3acdb96c9
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-01-17 19:58:27 -0300

    Criada a página com a pergunta a ser respondida

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const { raw } = require("body-parser");
 //DATABASE
 
 connection
@@ -20,7 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false })); //PERMITE O ENVIO DOS DADOS
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.render("index");
+  Pergunta.findAll({ raw: true, order:[
+    ['id', 'DESC'] //ASC = CRESCENTE || DESC = DECRESCENTE
+  ] }).then((perguntas) => {
+    res.render("index",{
+      perguntas: perguntas
+    });
+  });
 });
 
 app.get("/perguntar", (req, res) => {
@@ -30,10 +38,35 @@ app.get("/perguntar", (req, res) => {
 app.post("/salvarpergunta", (req, res) => {
   var titulo = req.body.titulo;
   var descricao = req.body.descricao;
-  res.send(
-    "Formulário recebido! titulo " + titulo + " " + " descricao" + descricao
-  );
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao,
+  }).then(() => {
+    res.redirect("/");
+  });
 });
+
+app.get("/pergunta/:id",(req, res)=>{
+  var id = req.params.id;
+  Pergunta.findOne({
+    where: {id: id}
+  }).then(pergunta =>{
+    if(pergunta != undefined){//Pergunta encontrada
+      res.render("pergunta",{
+        pergunta:pergunta
+      });
+
+    }else{//NÃO ENCONTRADA
+      res.redirect("/");
+
+    }
+
+  });
+
+});
+
+
+
 
 app.listen(8080, () => {
   console.log("APP RODANDO");
